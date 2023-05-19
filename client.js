b832a3425bf61586bc84b74aebca3ebfa5dea4cb
theblackmace <52634803+theblackmace@users.noreply.github.com>
2023-05-19 06:04:11 +0530

    2

--- client.js
+++ client.js
@@ -31,15 +31,27 @@ const contractAddress = '0x4CaEF52018EC1218B18D233eA0e366367EDea902';
 
 let MoodContract;
 let signer;
+let provider;
+let account;
 
 
-const provider = new ethers.providers.Web3Provider(window.ethereum, "mumbai");
+async function getAccount() {
+	const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+	  .catch((err) => {
+		if (err.code === 4001) {
+		  // EIP-1193 userRejectedRequest error
+		  // If this happens, the user rejected the connection request.
+		  console.log('Please connect to MetaMask.');
+		} else {
+		  console.error(err);
+		}
+	  });
+	account = accounts[0];
+}
+  
 
-provider.send("eth_requestAccounts", []).then(() => {
-    provider.listAccounts().then((accounts) => {
-        signer = provider.getSigner(accounts[0]);
-        MoodContract = new ethers.Contract(contractAddress, contractABI, signer);
-    });
+window.addEventListener('load', function() {
+	start();
 });
 
 document.getElementById('get-mood').addEventListener('click', function() {
@@ -50,6 +62,48 @@ document.getElementById('set-mood').addEventListener('click', function() {
     setMood();
 })
 
+async function start() {
+
+	getAccount();
+
+	const selectedChainId = await window.ethereum.request({ method: 'eth_chainId' });
+
+	if(selectedChainId === '0x13881'){
+		init();
+	} else {
+		try {
+			await window.ethereum.request({
+				method: 'wallet_switchEthereumChain',
+				params: [{ chainId: '0x13881'}],
+			});
+			  // refresh
+			window.location.reload();			
+		} catch {
+			window.ethereum.request({
+				method: "wallet_addEthereumChain",
+				params: [{
+					chainId: "0x13881",
+					rpcUrls: ["https://rpc.ankr.com/polygon_mumbai"],
+					chainName: "Mumbai Testnet",
+					nativeCurrency: {
+						name: "MATIC",
+						symbol: "MATIC",
+						decimals: 18
+					},
+					blockExplorerUrls: ["https://mumbai.polygonscan.com/"]
+				}]
+			});		
+		}
+	}
+}
+
+function init() {
+	provider = new ethers.providers.Web3Provider(window.ethereum);
+	signer = provider.getSigner(account);
+	MoodContract = new ethers.Contract(contractAddress, contractABI, signer);
+	console.log(MoodContract);
+}
+
 async function getMood() {
     const getMoodPromise = MoodContract.getMood();
     const Mood = await getMoodPromise;
@@ -59,6 +113,8 @@ async function getMood() {
 
 async function setMood() {
     const mood = document.getElementById('mood').value;
-    const setMoodPromise = MoodContract.setMood(mood);
-    await setMoodPromise;
+    const setMoodPromise = await MoodContract.setMood(mood);
+	const txReciept = await setMoodPromise.wait([confirms = 1]);
+	console.log(txReciept);
+	getMood();
 }
